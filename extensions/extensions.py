dbad15a23e1ce6003141391df4b97410038fcc84
bill <bill@pixelspoke.com>
2015-05-09 08:22:57 -0700

    Make weewx sleep until rpi set to reasonable date
    If the RTC dies and there is no network connection preventing NTP from
    setting the system date, this prevents weewx from using the rpi's wakeup
    date (Unix time 0!).

---
+++ extensions/extensions.py
@@ -0,0 +1,25 @@
+#
+#    Copyright (c) 2011 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+#    $Id: extensions.py 2749 2014-11-29 18:15:24Z tkeffer $
+#
+
+"""User extensions module
+
+This module is imported from the main executable, so anything put here will be
+executed before anything else happens. This makes it a good place to put user
+extensions.
+"""
+
+import locale
+# This will use the locale specified by the environment variable 'LANG'
+# Other options are possible. See:
+# http://docs.python.org/2/library/locale.html#locale.setlocale
+locale.setlocale(locale.LC_ALL, '')
+
+import time
+# Sleep until clock has a reasonable date (1 Jan 2015 or later)
+while time.time() < 1420099200:
+	time.sleep(0.5)
